fb816db1002804e0f3bc4d378820263281d94f30
Blakells <blake.bailey09@gmail.com>
2020-06-01 14:31:25 -0400

    7

--- app.js
+++ app.js
@@ -23,15 +23,14 @@ mongoose.connect(config.MONGODB_URI, ({useNewUrlParser: true, useUnifiedTopology
 })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname,'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  })
-
+})
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
