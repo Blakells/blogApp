5d4317d3ddc2db0f29f437cdd78c66f284f3b4d2
Blakells <blake.bailey09@gmail.com>
2020-06-01 10:26:11 -0400

    buildui

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b618c6757887d9edd3f77d19ecfc075.js"
+  "/precache-manifest.ab3c8053644485f06fbb6d2a8f791e2f.js"
 );
 
 self.addEventListener('message', (event) => {
